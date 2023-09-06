fcec7ebeddd5b50c5ebe72128816d82401616979
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-09-06 14:47:01 +0300

    test commit2

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
             <Skills/>
             <Works/>
             <Testimonials/>
+            {/*<Contacts/>*/}
             <Contacts/>
             <Slogan/>
             <Footer/>
